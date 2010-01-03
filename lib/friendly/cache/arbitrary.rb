ecfa85ed409c157ef877f915e1b0325764a10f89
James Golick <james@giraffesoft.ca>
2010-01-03 14:27:24 -0600

    Cache::Arbitrary#update

--- lib/friendly/cache/arbitrary.rb
+++ lib/friendly/cache/arbitrary.rb
@@ -6,11 +6,35 @@ module Friendly
         cache.set(cache_key(document), existing.push(document.id))
       end
 
+      def update(document)
+        old_key  = old_key(document)
+        existing = cache.get(old_key)
+        if existing
+          existing.delete(document.id)
+          cache.set(old_key, existing)
+        end
+        create(document)
+      end
+
       protected
-        def cache_key(document)
+        def make_key(document, &block)
           key = [klass.name, version]
-          fields.each { |f| key.push(f, document.send(f)) }
-          key.join("/")
+          fields.inject(key, &block).join("/")
+        end
+
+        def cache_key(document)
+          make_key(document) { |key, f| key + [f, document.send(f)] }
+        end
+
+        def old_key(document)
+          make_key(document) do |key, f|
+            key + [f, get_original_or_current_value(document, f)]
+          end
+        end
+
+        def get_original_or_current_value(document, field)
+          document.attribute_changed?(field) ?
+            document.attribute_was(field) : document.send(field)
         end
     end
   end
--- spec/unit/cache/aribtrary_spec.rb
+++ spec/unit/cache/aribtrary_spec.rb
@@ -1,6 +1,9 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 
 describe "Friendly::Cache::Arbitrary" do
+  def cache_key(name)
+    ["User", 0, "name", name].join("/")
+  end
   before do
     @klass     = stub(:name => "User")
     @memcached = stub(:set => nil, :get => nil)
@@ -8,7 +11,7 @@ describe "Friendly::Cache::Arbitrary" do
     @cache     = Friendly::Cache::Arbitrary.new(@klass, @fields, {}, @memcached)
     @id        = Friendly::UUID.new
     @doc       = stub(:name => "Bond", :id => @id)
-    @key       = ["User", 0, "name", "Bond"].join("/")
+    @key       = cache_key("Bond")
   end
 
   describe "#create" do
@@ -30,8 +33,42 @@ describe "Friendly::Cache::Arbitrary" do
       end
 
       it "adds the object to the array" do
-        key = ["User", 0, "name", "Bond"].join("/")
-        @memcached.should have_received(:set).with(key, [@other_id, @id])
+        @memcached.should have_received(:set).with(@key, [@other_id, @id])
+      end
+    end
+  end
+
+  describe "#update" do
+    before do
+      @new_key = cache_key("James")
+      @doc = stub(:name    => "James", :attribute_was => "Bond", 
+                  :changed => [:name], :id            => @id,
+                  :attribute_changed? => true)
+    end
+
+    describe "when there's already something in cache" do
+      before do
+        @memcached.stubs(:get).with(@key).returns([@id])
+        @cache.update(@doc)
+      end
+
+      it "removes the object from the cache index it used to be in" do
+        @memcached.should have_received(:set).with(@key, [])
+      end
+
+      it "adds the object from the new cache key" do
+        @memcached.should have_received(:set).with(@new_key, [@id])
+      end
+    end
+
+    describe "when there's nothing in cache already" do
+      before do
+        @cache.update(@doc)
+      end
+
+      it "adds the object from the new cache key" do
+        @memcached.should have_received(:set).once
+        @memcached.should have_received(:set).with(@new_key, [@id])
       end
     end
   end
