f799cf242d38de485262554f7e4af0f5cfd44a44
James Golick <james@giraffesoft.ca>
2010-01-24 10:01:41 -0800

    return the value of the block when benchmarking

--- lib/friendly/benchmarking.rb
+++ lib/friendly/benchmarking.rb
@@ -4,8 +4,10 @@ module Friendly
   module Benchmarking
     def benchmark(title, level = Logger::DEBUG)
       if logger.level <= level
-        ms = benchmark_klass.ms { yield }
+        to_return = nil
+        ms        = benchmark_klass.ms { to_return = yield }
         logger.add(level, title.gsub(/:time/, "(%.1fms)" % ms))
+        to_return
       else
         yield
       end
--- spec/unit/benchmarking_spec.rb
+++ spec/unit/benchmarking_spec.rb
@@ -17,14 +17,18 @@ describe "Friendly::Benchmarking" do
       @logger          = stub(:level => Logger::DEBUG, :add => true)
       @benchmark_klass = Class.new { def ms; yield; 5; end }.new
       @benchmarker     = BenchmarksStuff.new(@logger, @benchmark_klass)
-      @benchmarker.benchmark("User created. :time") do
-
+      @return_value    = @benchmarker.benchmark("User created. :time") do
+        5
       end
     end
 
     it "logs and substitutes the time in to the title" do
       @logger.should have_received(:add).
                       with(Logger::DEBUG, "User created. (5.0ms)")
+    end
+
+    it "returns the return value of the block" do
+      @return_value.should == 5
     end
   end
 
@@ -38,6 +42,10 @@ describe "Friendly::Benchmarking" do
       ran = false
       @benchmarker.benchmark("User created.") { ran = true }
       ran.should be_true
+    end
+
+    it "returns the return value of the block" do
+      @benchmarker.benchmark("User created.") { true }.should == true
     end
   end
 end
