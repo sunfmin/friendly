c72002b31aaaa25e05bbc6b878b45f03336ac623
James Golick <james@giraffesoft.ca>
2010-01-23 17:38:44 -0800

    mess with the logger formatting a little

--- lib/friendly/logger.rb
+++ lib/friendly/logger.rb
@@ -4,11 +4,12 @@ module Friendly
   class Logger < ::Logger
     def initialize(*args)
       super
-      @default_formatter = Formatter.new
+      @default_formatter   = Formatter.new
+      self.datetime_format = "%Y-%d-%m %H:%M:%S"
     end
 
     class Formatter < ::Logger::Formatter
-      Format = "[%s] [%s] : %s\n"
+      Format = "[%s] [%s]: %s\n"
 
       def call(severity, time, progname, msg)
         output_message = msg2str(msg)
